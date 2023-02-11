a17f57e624737fa5955d518fccef5d9b0caa0a17
daniel ben avi <daniel.ben.avi10@gmail.com>
2023-02-11 17:18:47 +0200

    Update: replace recycleView of med with text View
    TODO:
    add choose doc and request from him

--- app/src/main/java/com/example/healthmanagementorganization/Fragments/Patient/MedicinesSearchFragment.java
+++ app/src/main/java/com/example/healthmanagementorganization/Fragments/Patient/MedicinesSearchFragment.java
@@ -1,23 +1,23 @@
 package com.example.healthmanagementorganization.Fragments.Patient;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.healthmanagementorganization.General.General;
 import com.example.healthmanagementorganization.Model.Medicine;
 import com.example.healthmanagementorganization.R;
-import com.example.healthmanagementorganization.Recyclerviews.RV_medicines_adapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,17 +25,14 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class MedicinesSearchFragment extends Fragment {
 
     private AppCompatAutoCompleteTextView search_med_ACACTV_search_bar;
-    private RecyclerView search_med_RV_med_list;
-
-
+    private AppCompatTextView search_med_ACTV_med_name, search_med_ACTV_med_price;
+    private AppCompatButton search_med_ACB_req_med;
     private DatabaseReference databaseReference;
-    private List<Medicine> items;
 
 
     @Override
@@ -44,12 +41,8 @@ public class MedicinesSearchFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_medicines_search, container, false);
         findViews(view);
         initViews();
-        initData();
 
         loadData();
-
-
-        search_med_RV_med_list.setLayoutManager(new LinearLayoutManager(getContext()));
 
 
         return view;
@@ -57,7 +50,6 @@ public class MedicinesSearchFragment extends Fragment {
 
     private void loadData() {
         databaseReference = FirebaseDatabase.getInstance().getReference(General.FB_Medicine);
-        search_med_RV_med_list.setAdapter(new RV_medicines_adapter(getContext(), items));
 
 
         ValueEventListener event = new ValueEventListener() {
@@ -80,18 +72,16 @@ public class MedicinesSearchFragment extends Fragment {
         if (snapshot.exists()) {
             for (DataSnapshot d : snapshot.getChildren()) {
                 Medicine m = d.getValue(Medicine.class);
-//                items.add(m);
+                assert m != null;
                 names.add(m.getName());
             }
 
             ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, names);
             search_med_ACACTV_search_bar.setAdapter(adapter);
-            search_med_ACACTV_search_bar.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    String name = search_med_ACACTV_search_bar.getText().toString();
-                    searchUser(name);
-                }
+            search_med_ACACTV_search_bar.setOnItemClickListener((parent, view, position, id) -> {
+                String name = search_med_ACACTV_search_bar.getText().toString();
+
+                searchUser(name);
             });
 
 
@@ -102,15 +92,16 @@ public class MedicinesSearchFragment extends Fragment {
 
     private void searchUser(String name) {
         databaseReference.orderByChild("name").equalTo(name).addListenerForSingleValueEvent(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
-                    items.clear();
                     for (DataSnapshot ds : snapshot.getChildren()) {
                         Medicine m = ds.getValue(Medicine.class);
-                        items.add(m);
+                        assert m != null;
+                        search_med_ACTV_med_name.setText("" + m.getName());
+                        search_med_ACTV_med_price.setText("" + m.getPrice());
                     }
-                    search_med_RV_med_list.setAdapter(new RV_medicines_adapter(getContext(), items));
                 }
             }
 
@@ -121,15 +112,17 @@ public class MedicinesSearchFragment extends Fragment {
         });
     }
 
-    private void initData() {
-        items = new ArrayList<Medicine>();
-    }
 
     private void initViews() {
+        search_med_ACB_req_med.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "TODO - request med", Toast.LENGTH_SHORT).show();
+        });
     }
 
     private void findViews(View view) {
-        search_med_RV_med_list = view.findViewById(R.id.search_med_RV_med_list);
         search_med_ACACTV_search_bar = view.findViewById(R.id.search_med_ACACTV_search_bar);
+        search_med_ACTV_med_name = view.findViewById(R.id.search_med_ACTV_med_name);
+        search_med_ACTV_med_price = view.findViewById(R.id.search_med_ACTV_med_price);
+        search_med_ACB_req_med = view.findViewById(R.id.search_med_ACB_req_med);
     }
 }
